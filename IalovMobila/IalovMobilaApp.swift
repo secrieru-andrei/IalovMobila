93a9a0f353315f8c5bc5c9669497224bf2cadfb9
Secrieru Andrei <secrieru.andrei@yahoo.com>
2022-09-21 09:17:57 +0300

    Starter Project

---
+++ IalovMobila/IalovMobilaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IalovMobilaApp.swift
+//  IalovMobila
+//
+//  Created by Secrieru Andrei on 21.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct IalovMobilaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
